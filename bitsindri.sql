fc73b0008c2b6eed8a575f5a1d79b9779bc62943
Ashish Kumar Mandal <666kmandal@gmail.com>
2021-06-08 15:57:55 +0530

    data base updated

--- bitsindri.sql
+++ bitsindri.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2021 at 08:29 PM
+-- Generation Time: Jun 08, 2021 at 12:26 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.13
 
@@ -183,7 +183,8 @@ INSERT INTO `notice` (`notice_id`, `msg`, `link`, `file`, `start_date_time`, `en
 (11, 'Notice for B.Tech 2nd sem form filling (JUT, Ranchi) session 2019-20- (posted on 8-1-2021)Notice for B.Tech 2nd sem form filling (JUT, Ranchi) session 2019-20.', 'https://bitsindri.ac.in/docs/Notice%20for%20B.Tech%202nd%20sem%20form%20filling%20(JUT,%20Ranchi)%20session%202019-20.pdf', NULL, '2021-02-01 18:30:00', '2021-03-07 14:29:00', 1, '2021-02-26 14:32:23', '2021-02-26 14:30:09'),
 (12, 'Notice for Tentative date of B.Tech 3rd Semester Examination 2020', 'https://bitsindri.ac.in/docs/Notice%20for%20tentative%C2%A0date%20of%20B.Tech%203rd%20Semester%20Examination%202020.pdf', NULL, '2021-02-25 18:30:00', '2021-03-14 14:31:00', 1, '2021-02-26 14:31:29', '2021-02-26 14:31:29'),
 (13, 'Consent for students of B TECH  3 rd and 4th year and M TECH 1st and 2nd year for residing in Hostel  (Posted on 25-2-2021)Consent for students of B TECH 3 rd and 4th year and M TECH 1st and 2nd year for residing in Hostel.', 'https://bitsindri.ac.in/docs/Consent%20for%20Hostels.pdf', NULL, '2021-02-24 18:30:00', '2021-03-14 14:33:00', 1, '2021-02-26 14:33:47', '2021-02-26 14:33:47'),
-(14, 'Notice for GATE Scholarship of M.Tech 1st sem 2020-21 students  (Posted on 26-2-2021)Notice for GATE Scholarship of M.Tech 1st sem 2020-21 students.', 'https://bitsindri.ac.in/docs/gatescholarshipnotice-26-2-2021.PDF', NULL, '2021-02-28 18:30:00', '2021-03-14 14:35:00', 1, '2021-02-26 14:35:31', '2021-02-26 14:35:31');
+(14, 'Notice for GATE Scholarship of M.Tech 1st sem 2020-21 students  (Posted on 26-2-2021)Notice for GATE Scholarship of M.Tech 1st sem 2020-21 students.', 'https://bitsindri.ac.in/docs/gatescholarshipnotice-26-2-2021.PDF', NULL, '2021-02-28 18:30:00', '2021-03-14 14:35:00', 1, '2021-02-26 14:35:31', '2021-02-26 14:35:31'),
+(16, 'college viode', 'https://www.youtube.com/watch?v=BS6mAxCwP0E', NULL, '2021-06-06 18:30:00', '2021-07-01 09:42:00', 1, '2021-06-07 09:42:21', '2021-06-07 09:42:21');
 
 -- --------------------------------------------------------
 
@@ -204,7 +205,6 @@ CREATE TABLE `slider` (
 --
 
 INSERT INTO `slider` (`sdr_id`, `file`, `heading`, `description`, `usr_id`) VALUES
-(1, '01032021175126.jpg', 'Welcome to BIT Sindri', 'The main aim of the institute is to cater to the needs of the nation for technological manpower development and research programs comparable to the best of the world. The college today offers B.Tech courses in 10 disciplines of engineering namely Mechanical, Electrical, Metallurgy, Production, Chemical, Electronics & Communications, Civil, Mining, Computer Science, Information technology, having NBA accreditation.', 1),
 (2, '01032021181736.jpg', '', '', 1);
 
 -- --------------------------------------------------------
@@ -236,7 +236,7 @@ INSERT INTO `teacher_contact` (`tchr_id`, `name`, `mob`, `email`, `desn_fk`, `de
 (4, 'Prof. Parbati Mahanto', '9470978517', 'pmahanto.it@bitsindri.ac.in', 3, 6, 1, '2021-03-09 18:32:00', '2021-03-09 18:32:00'),
 (5, 'Prof. Rajiv Ranjan', '9507420028', 'rajivranjan.it@bitsindri.ac.in', 3, 6, 1, '2021-03-09 18:32:53', '2021-03-09 18:32:53'),
 (6, 'Dr. Amit Kumar Gupta', '9835785852', 'hod.che@bitsindri.ac.in', 5, 1, 1, '2021-03-09 18:34:13', '2021-03-09 18:34:13'),
-(7, 'Dr. Amar Kumar', '9334281501', 'akumar.che@bitsindri.ac.in', 3, 1, 1, '2021-03-09 18:34:51', '2021-03-09 18:34:51');
+(7, 'Dr. Amar Kumar', '9334281501', 'akumar.che@bitsindri.ac.in', 3, 1, 1, '2021-03-09 18:34:51', '2021-03-10 05:07:32');
 
 -- --------------------------------------------------------
 
@@ -263,6 +263,27 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`usr_id`, `desn_fk`, `dept_fk`, `name`, `mobile`, `email`, `password`, `updated`, `created`) VALUES
 (1, 1, 11, 'Ashish Kumar Mandal', '8228972907', '666kmandal@gmail.com', '829bc05e9555023e3c988c5637fec5b2', '2021-02-26 19:24:30', '2021-02-26 19:24:30'),
 (2, 2, 6, 'Ashish Kumar', '9155035586', 'abc@gmail.com', '25f9e794323b453885f5181f1b624d0b', '2021-02-26 19:26:00', '2021-02-26 19:26:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `video`
+--
+
+CREATE TABLE `video` (
+  `video_id` int(11) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `link` varchar(150) NOT NULL,
+  `updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `video`
+--
+
+INSERT INTO `video` (`video_id`, `title`, `link`, `updated`, `created`) VALUES
+(4, 'bit', 'https://youtu.be/7wQ9V7mBY0o', '2021-06-07 10:45:03', '2021-06-07 10:45:03');
 
 --
 -- Indexes for dumped tables
@@ -328,6 +349,12 @@ ALTER TABLE `user`
   ADD KEY `dept_fk` (`dept_fk`);
 
 --
+-- Indexes for table `video`
+--
+ALTER TABLE `video`
+  ADD PRIMARY KEY (`video_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -365,7 +392,7 @@ ALTER TABLE `message`
 -- AUTO_INCREMENT for table `notice`
 --
 ALTER TABLE `notice`
-  MODIFY `notice_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `notice_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `slider`
@@ -377,13 +404,19 @@ ALTER TABLE `slider`
 -- AUTO_INCREMENT for table `teacher_contact`
 --
 ALTER TABLE `teacher_contact`
-  MODIFY `tchr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `tchr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `usr_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `video`
+--
+ALTER TABLE `video`
+  MODIFY `video_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
